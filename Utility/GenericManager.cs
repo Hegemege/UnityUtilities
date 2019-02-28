8866e25fc924d01a072ac080166a73d6c02378e9
Henri Niva <henri.a.niva@gmail.com>
2019-02-28 19:30:44 +0200

    Allow inheriting class to detect singleton re-creation

--- Utility/GenericManager.cs
+++ Utility/GenericManager.cs
@@ -7,15 +7,17 @@ public abstract class GenericManager<T> : MonoBehaviour
     // Singleton setup
     public static T Instance { get; protected set; }
 
-    protected virtual void InitializeSingleton(T parent)
+    protected virtual bool InitializeSingleton(T parent)
     {
         if (Instance != null)
         {
             Destroy(gameObject);
-            return;
+            return false;
         }
 
         DontDestroyOnLoad(gameObject);
         Instance = parent;
+
+        return true;
     }
 }
